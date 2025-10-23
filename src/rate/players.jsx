3b4022af9e8c5d386c81444b3e2cc426e2231075
Gracee Putnam <gracee.putnam@gmail.com>
2025-10-22 23:00:39 -0600

    fixing the players.jsx

--- src/rate/players.jsx
+++ src/rate/players.jsx
@@ -1,17 +1,58 @@
-const [events, setEvent] = React.useState([]);
+import React from 'react';
 
-React.useEffect(() => {
-  GameNotifier.addHandler(handleGameEvent);
+import { GameEvent, GameNotifier } from './gameNotifier';
+import './players.css';
 
-  return () => {
-    GameNotifier.removeHandler(handleGameEvent);
-  };
-});
+export function Players(props) {
+  const userName = props.userName;
 
-function handleGameEvent(event) {
-  let newEvents = [event, ...events];
-  if (newEvents.length > 10) {
-    newEvents = newEvents.slice(1, 10);
+  const [events, setEvent] = React.useState([]);
+
+  React.useEffect(() => {
+    GameNotifier.addHandler(handleGameEvent);
+
+    return () => {
+      GameNotifier.removeHandler(handleGameEvent);
+    };
+  }, []);
+
+  function handleGameEvent(event) {
+    setEvent((prevEvents) => {
+      let newEvents = [event, ...prevEvents];
+      if (newEvents.length > 10) {
+        newEvents = newEvents.slice(1, 10);
+      }
+      return newEvents;
+    });
   }
-  setEvent(newEvents);
+
+  function createMessageArray() {
+    const messageArray = [];
+    for (const [i, event] of events.entries()) {
+      let message = 'unknown';
+      if (event.type === GameEvent.End) {
+        message = `scored ${event.value.score}`;
+      } else if (event.type === GameEvent.Start) {
+        message = `started a new game`;
+      } else if (event.type === GameEvent.System) {
+        message = event.value.msg;
+      }
+
+      messageArray.push(
+        <div key={i} className='event'>
+          <span className={'player-event'}>{event.from.split('@')[0]}</span>
+          {message}
+        </div>
+      );
+    }
+    return messageArray;
+  }
+
+  return (
+    <div className='players'>
+      Player
+      <span className='player-name'>{userName}</span>
+      <div id='player-messages'>{createMessageArray()}</div>
+    </div>
+  );
 }
