43dc941c2de1d89e52d64e1cfa0aed4066ba410e
Gracee Putnam <gracee.putnam@gmail.com>
2025-11-22 21:33:12 -0700

    finally changing the rate for ws

--- src/rate/rate.jsx
+++ src/rate/rate.jsx
@@ -5,7 +5,7 @@ import { RateGame } from './rateGame';
 
 export function Rate(props) {
   return (
-    <main className="bg-secondary rate-main" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+    <main className="bg-secondary rate-main" style={{ display: "flex",  gap: "32px", flexWrap: "wrap", alignItems: "flex-start", justifyContent: "flex-start" }}>
       {/* Community/event activity feed */}
       <div style={{ flex: "1 280px", maxWidth: 300, margin: "32px 32px 0 0" }}>
         <Players userName={props.userName} />
